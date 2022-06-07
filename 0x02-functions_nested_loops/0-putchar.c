a2733f8306143c1e083ff3e41e5e8c7e925195e9
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-06-07 16:09:50 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letters = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(letters)
+			letters++;
+	}
+	_putchar('\n');
+}
