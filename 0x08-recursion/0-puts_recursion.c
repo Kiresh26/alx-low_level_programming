24d7e6c38d433d3d9bfa0e367b688045d1e22f6e
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-06-22 07:51:04 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print string follow by new line
+ * @s: string to print
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	char s;
+
+	puts(s);
+
+	return (0);
+}
