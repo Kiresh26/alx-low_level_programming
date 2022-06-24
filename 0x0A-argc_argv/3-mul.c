d14d2b6556ba1e9a294d6371849f076e2b5f85a5
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-06-24 16:00:38 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point, multiply numbers
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argv[1] && argv[2])
 	{
-		printf("%d\n", atoi(argv[1] * atoi(argv[2])));
+		printf("%d\n", atoi(argv[1]) * (atoi(argv[2])));
 		return (0);
 	}
 	else
