7ed5cb95c7b278feb7c829660d92b72657b06a5b
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-06-28 15:16:03 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * free_grid - use free function on previous grid
  * @grid: 2 dim. array to free
- * @height: input height (size)
+ * @height: input height
+ *
  * Return: free'd grid
- */
+ **/
+
 void free_grid(int **grid, int height)
 {
 	int i;
@@ -17,6 +20,7 @@ void free_grid(int **grid, int height)
 		{
 			free(grid[i]);
 		}
+
 		free(grid);
 	}
 }
