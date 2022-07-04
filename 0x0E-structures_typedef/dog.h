4489b189728f87678b9daf1ac0ef815bc628df98
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-07-04 12:11:13 -0700

    0. poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG.H
+#define DOG.H
+
+/**
+ * struct dog - defines a new tpye dog
+ * @age: age of dog, int
+ * @name: name of dog, char
+ * @owner: owner of dog, char
+ */
+
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif /*DOG.H*/
